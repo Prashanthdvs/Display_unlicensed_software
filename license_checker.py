9747dcb34e4da36d185aaf5dcc765ab3de32c45e
Prashanthdvs <86160300+Prashanthdvs@users.noreply.github.com>
2023-06-20 20:39:13 +0530

    Add files via upload

--- license_checker.py
+++ license_checker.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import subprocess
 import webbrowser
+import tkinter.font as font
 
 def scan_installed_software():
 # Use system APIs or command-line tools to scan for installed software
@@ -19,11 +20,10 @@ def scan_installed_software():
 
 def verify_license(software_list):
     # list of licensed software for reference
-    licensed_software = ['Python 3.10.10 Test Suite (64-bit symbols)', 'Microsoft DCF MUI (English) 2016', 'Microsoft Office Professional Plus 2016', 'Microsoft OneNote MUI (English) 2016', 'Microsoft Office 32-bit Components 2016', 'Microsoft Office Shared 32-bit MUI (English) 2016', 'Microsoft Office OSM MUI (English) 2016', 'Microsoft Office OSM UX MUI (English) 2016', 'Microsoft InfoPath MUI (English) 2016', 'Microsoft Access MUI (English) 2016', 'Microsoft Office Shared Setup Metadata MUI (English) 2016', 'Microsoft Excel MUI (English) 2016', 'Microsoft Access Setup Metadata MUI (English) 2016', 'Microsoft PowerPoint MUI (English) 2016', 'Microsoft Publisher MUI (English) 2016', 'Microsoft Outlook MUI (English) 2016', 'Microsoft Groove MUI (English) 2016', 'Microsoft Word MUI (English) 2016', 'Microsoft Skype for Business MUI (English) 2016', 'Microsoft Office Proofing (English) 2016', 'Microsoft Office Shared MUI (English) 2016', 'Microsoft Office Proofing Tools 2016 - English', 'Herramientas de correcci\\xa2n de Microsoft Office 2016: espa\\xa4ol', 'Outils de v\\x82rification linguistique 2016 de Microsoft Office\\xff- Fran\\x87ais', 'Microsoft Visual C++ 2022 X64 Minimum Runtime - 14.32.31332'] # Example list of licensed software
+    licensed_software = ['Python 3.10.10 Test Suite (64-bit symbols)', 'Microsoft DCF MUI (English) 2016', 'Microsoft Office Professional Plus 2016', 'Microsoft OneNote MUI (English) 2016', 'Microsoft Office 32-bit Components 2016', 'Microsoft Office Shared 32-bit MUI (English) 2016', 'Microsoft Office OSM MUI (English) 2016', 'Microsoft Office OSM UX MUI (English) 2016', 'Microsoft InfoPath MUI (English) 2016', 'Microsoft Access MUI (English) 2016', 'Microsoft Office Shared Setup Metadata MUI (English) 2016', 'Microsoft Excel MUI (English) 2016', 'Microsoft Access Setup Metadata MUI (English) 2016', 'Microsoft PowerPoint MUI (English) 2016', 'Microsoft Publisher MUI (English) 2016', 'Microsoft Outlook MUI (English) 2016', 'Microsoft Groove MUI (English) 2016', 'Microsoft Word MUI (English) 2016', 'Microsoft Skype for Business MUI (English) 2016', 'Microsoft Office Proofing (English) 2016', 'Microsoft Office Shared MUI (English) 2016', 'Microsoft Office Proofing Tools 2016 - English', 'Herramientas de correcci\\xa2n de Microsoft Office 2016: espa\\xa4ol', 'Outils de v\\x82rification linguistique 2016 de Microsoft Office\\xff- Fran\\x87ais', 'Microsoft Visual C++ 2022 X64 Minimum Runtime - 14.32.31332', 'BigFix Client','Bloxoner Endpoint','Cisco AnyConnect Diagnostics and Reporting Tool','Cisco AnyConnect ISE Compliance Module','Cisco AnyConnect ISE Posture Module','Cisco AnyConnect Network Access Manager','Cisco AnyConnect Secure Mobility Client','Cisco AnyConnect Start Before Login Module','Cyberark Endpoint Protection','Healing Station Admin','Healing Station Agent','Nippon Buddy','Symantec Endpoint Protection','Symantec Wss Agent','TITUS Classification (64 bit)','BloxOner Endpoint','Trellix Endpoint Security (HX) Agent'] # Example list of licensed software
 
     # Compare the list of installed software with the licensed software database
     unlicensed_software = [software for software in software_list if software not in licensed_software]
-
     return unlicensed_software
 
 def start_button_clicked():
@@ -53,7 +53,9 @@ def display_unlicensed_software(unlicensed_software):
 window = tk.Tk()
 
 # Create UI elements
-start_button = tk.Button(window, text='Start', command=start_button_clicked)
+myFont = font.Font(size=18)
+start_button = tk.Button(window, text='Start',height=1, width=8, command=start_button_clicked)
+start_button['font']= myFont
 display_area = tk.Text(window, height=40, width=70)
 
 # Place UI elements in the window
